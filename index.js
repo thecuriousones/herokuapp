b9b8e16c4e8454fb35f8c1e11211ff35daf88ae3
thecuriousones <tamaraleonard46@gmail.com>
2021-08-11 20:35:21 -0400

    deloopment push

---
+++ index.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv').config()
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const port = process.env.PORT || 5000
+
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`)
+})
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/*", (_,res)=>{
+    res.json({data: 'The API is working'})
+})
+
+console.log("It's alive")
+console.log(__dirname)
+console.log(__filename)
+console.log(process.env.USERNAME)
+console.log(process.env.PORT)
+console.log(process.env.GREET)
+console.log(process.env.FOOD)
